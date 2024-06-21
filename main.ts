31749f7bbeb8d4834d1ac8cb0d465aa5c47d6ceb
Kazu <83614559+kazu-321@users.noreply.github.com>
2024-06-21 15:57:41 +0900

    課題

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+    if (counter == 3) {
+        counter = 0
+        basic.showIcon(IconNames.Heart)
+    } else if (counter == 1) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
 })
+let counter = 0
+counter = 0
+basic.showIcon(IconNames.Heart)

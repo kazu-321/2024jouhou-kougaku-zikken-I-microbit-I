10a87d8f5e042ce71c0f09cc5e5249c4d504e213
Kazu <83614559+kazu-321@users.noreply.github.com>
2024-06-21 16:02:45 +0900

    課題

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+    basic.showNumber(counter)
 })
+input.onButtonPressed(Button.B, function () {
+    counter = 0
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
+basic.showNumber(counter)

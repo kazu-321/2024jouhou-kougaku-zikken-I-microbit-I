f79f4cdad0316a1b1ca1140496764bb1a4e92bc2
Kazu <83614559+kazu-321@users.noreply.github.com>
2024-06-21 15:52:48 +0900

    課題1

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.West)
 })
